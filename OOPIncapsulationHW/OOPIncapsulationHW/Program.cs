e2ac0ca2e7ed4e852505211968d01481cb479606
Andrew <shikigamori@gmail.com>
2023-05-30 12:46:51 +0300

    Finished OOP Incapsulation HW

--- OOPIncapsulationHW/OOPIncapsulationHW/Program.cs
+++ OOPIncapsulationHW/OOPIncapsulationHW/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace OOPIncapsulationHW
+﻿using System.Text.RegularExpressions;
+
+namespace OOPIncapsulationHW
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+/*            Cathedra cathedra = new Cathedra();
+            cathedra.groups = new Group[]
+            {
+                new Group() { Name = "Firsty", Cathedra = cathedra},
+                new Group() { Name = "Secondy", Cathedra = cathedra},
+                new Group() { Name = "Thirdy", Cathedra = cathedra}
+            };
+
+            cathedra.AddGroup(new Group() { Name = "NEWNEWNEW", Cathedra = cathedra });
+
+            cathedra.RemoveGroup(new Group() { Name = "Secondy", Cathedra = cathedra });*/
         }
     }
 }
---
+++ OOPIncapsulationHW/OOPIncapsulationHW/Room.cs
@@ -0,0 +1,20 @@
+﻿namespace OOPIncapsulationHW
+{
+    public class Room
+    {
+        public string Name { get; set; }
+        private Lesson Lesson { get; set; }
+
+        public Room(string name, Lesson lesson)
+        {
+            Name = name;
+            Lesson = lesson;
+        }
+
+        // set lesson
+        public void SetLesson(Lesson newLesson)
+        {
+            this.Lesson = newLesson;
+        }
+    }
+}

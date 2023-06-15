3d6aa211cf352047cc17bc65e95a0d165b9af4ee
Andrew <shikigamori@gmail.com>
2023-06-15 18:39:02 +0300

    Created new sln

---
+++ GenericsHW/GenericsHW/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

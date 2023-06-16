8ab26d68c57a5753347803796cf348ba1dffa05e
Andrew <shikigamori@gmail.com>
2023-06-16 19:12:52 +0300

    Created stack class

--- GenericsHW/GenericsHW/Program.cs
+++ GenericsHW/GenericsHW/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            Console.WriteLine(stack.Count);
+
+            Console.WriteLine(stack.Peek());
+
+            Console.WriteLine(stack.Pop());
+
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Count);
         }
     }
 }

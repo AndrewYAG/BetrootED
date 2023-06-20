b0dba943052f7ee8d8abe85366222179855169e5
Andrew <shikigamori@gmail.com>
2023-06-20 18:55:21 +0300

    Finished Collections HW
    finished hw to create vote app

---
+++ CollectionsHW/CollectionsHW/VoteTopic.cs
@@ -0,0 +1,59 @@
+﻿namespace CollectionsHW
+{
+    public class VoteTopic : IChangableTitle
+    {
+        public string Title { get; private set; }
+        public Dictionary<int, Vote> VoteList { get; private set; }
+
+        public VoteTopic(string title, Dictionary<int, Vote> voteList)
+        {
+            Title = title;
+            VoteList = voteList;
+        }
+        public VoteTopic(string title) : this(title, new Dictionary<int, Vote>())
+        {
+        }
+
+        public Vote this[int index]
+        {
+            get { return VoteList[index]; }
+            private set { VoteList[index] = value; }
+        }
+
+        public void AddVoteOption(Vote newVoteVariant)
+        {
+            int nextKey = VoteList.Count + 1;
+            VoteList.Add(nextKey, newVoteVariant);
+        }
+
+        public void AddVoteOption(string newVoteVariantTitle)
+        {
+            int nextKey = VoteList.Count + 1;
+            VoteList.Add(nextKey, new Vote(newVoteVariantTitle));
+        }
+
+        public void ShowVoteOptions()
+        {
+            Console.WriteLine($"\nYou entered vote topic: {Title}\n");
+            Console.WriteLine("You have following vote options:");
+            foreach (var voteVariant in VoteList)
+            {
+                Console.WriteLine($"{voteVariant.Key} -- " + voteVariant.Value.Title);
+            }
+        }
+
+        public void ShowVoteResults()
+        {
+            Console.WriteLine("\nVote results:");
+            foreach (var voteVariant in VoteList)
+            {
+                Console.WriteLine($"№{voteVariant.Key} -- " + voteVariant.Value.Title + 
+                    $" [{voteVariant.Value.NumOfVotes} Votes]");
+            }
+        }
+
+        public void MakeVote(int numOfVoteOptionToVote) => VoteList[numOfVoteOptionToVote].IncreaseNumOfVotesByOne();
+
+        public void ChangeTitle(string newTitle) => Title = newTitle;
+    }
+}
---
+++ CollectionsHW/CollectionsHW/VoteTopicController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsHW
+{
+    public static class VoteTopicController
+    {
+        public static void StartVoteProcess(VoteTopic vote)
+        {
+            while (true)
+            {
+                PrintMenu();
+
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.D1:
+                        vote.ShowVoteOptions();
+
+                        Console.Write("Enter vote num to vote for: ");
+                        int numOfTopic = int.Parse(Console.ReadLine());
+
+                        if (numOfTopic <= 0 || numOfTopic > (vote.VoteList.Count))
+                            throw new ArgumentException("Invalid num of vote topic to vote for. " +
+                                $"It must be in range [0; {vote.VoteList.Count + 1}]");
+
+                        vote.MakeVote(numOfTopic);
+                        break;
+
+                    case ConsoleKey.D2:
+                        vote.ShowVoteResults();
+                        break;
+
+                    case ConsoleKey.D3:
+                        vote.ShowVoteOptions();
+
+                        Console.Write("Enter vote option to add: ");
+                        var newOption = Console.ReadLine();
+
+                        if (String.IsNullOrWhiteSpace(newOption))
+                            throw new ArgumentException("New vote option is null, empty or whitespace. It must contain a value!");
+
+                        vote.AddVoteOption(newOption);
+                        break;
+
+                    case ConsoleKey.D4:
+                        return;
+
+                        default: 
+                        throw new ArgumentException($"You pressed wrong button. It must be in range 1-4!");
+                }
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("Welcome to vote menu! Press button to choose option from the list bellow:");
+            Console.WriteLine("1. Make Vote\n2. Show Vote Results\n3. Add Vote Option\n4. Exit");
+        }
+    }
+}

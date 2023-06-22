7df4e164e709fc142a05f06b61f08614ec9d0ffa
Andrew <shikigamori@gmail.com>
2023-06-22 17:53:13 +0300

    Created new sln

---
+++ DelegateHw/DelegateHw/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DelegateHw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
